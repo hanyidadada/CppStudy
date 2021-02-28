700375b0bc50eb68a1a4177902f672dcd8dc162b
hanyidadada <44640043+hanyidadada@users.noreply.github.com>
2021-02-28 22:15:20 +0800

    20210228

--- gitinit.bat
+++ gitinit.bat
@@ -1,7 +1,7 @@
 ::初始化git
 git init
 ::添加远程路径
-git remote add origin https://github.com/hanyidadada/CppStudy
+git remote add origin git@github.com:hanyidadada/CppStudy.git
 ::保存用户名密码
 git config --global credential.helper store
 
