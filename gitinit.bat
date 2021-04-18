0188a15305102e826a4161ea5d0275a7c3c6c8ac
hanyidadada <44640043+hanyidadada@users.noreply.github.com>
2021-04-18 17:46:47 +0800

    练习

--- gitinit.bat
+++ gitinit.bat
@@ -1,5 +1,7 @@
+chcp 65001
 ::初始化git
 git init
 ::添加远程路径
 git remote add origin https://github.com/hanyidadada/CppStudy
 
+git branch --set-upstream-to=origin/Chapter9 master
--- upload.bat
+++
@@ -1,9 +0,0 @@
-chcp 65001
-::添加到暂存区
-git add .
-::第一次提交
-git commit -m "练习"
-::同步远程仓库
-git pull origin Chapter9:master --allow-unrelated-histories
-::推送本地分支到远程
-git push -f origin master:Chapter9
