4f397779cd4f4aa6f6d538cd372c3777e616aee3
hanyidadada <44640043+hanyidadada@users.noreply.github.com>
2021-04-20 21:08:12 +0800

    整理

--- upload.bat
+++
@@ -1,9 +0,0 @@
-chcp 65001
-::添加到暂存区
-git add .
-::第一次提交
-git commit -m "课后习题"
-::同步远程仓库
-git pull origin Chapter2:master --allow-unrelated-histories
-::推送本地分支到远程
-git push -u origin master:Chapter2
